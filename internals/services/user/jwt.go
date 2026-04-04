106c73dd114332fe2bac3bd3f1388f6b26cdf2ff
JagTheFriend <JagTheFriend12@gmail.com>
2026-04-04 20:23:29 +0530

    Use JWT for authentication

---
+++ internals/services/user/jwt.go
@@ -0,0 +1,57 @@
+package user
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v5"
+)
+
+var jwtSecret = []byte("super-secret-key") // move to env in prod
+
+type CustomClaims struct {
+	UserID string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(userID string) (string, error) {
+	claims := CustomClaims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func JWTMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c *echo.Context) error {
+		authHeader := c.Request().Header.Get("Authorization")
+
+		if authHeader == "" {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "missing token"})
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.ParseWithClaims(tokenStr, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "invalid token"})
+		}
+
+		claims := token.Claims.(*CustomClaims)
+
+		// store userID in context
+		c.Set("user_id", claims.UserID)
+
+		return next(c)
+	}
+}
