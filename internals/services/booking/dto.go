c397fd9adb4842f5d4b51fb664c5f4abdb414937
JagTheFriend <JagTheFriend12@gmail.com>
2026-04-05 10:05:51 +0530

    Created DTOs for booking route

--- internals/services/booking/dto.go
+++ internals/services/booking/dto.go
@@ -1,1 +1,12 @@
 package booking
+
+type CreateBookingRequest struct {
+	SeatID    string  `json:"seatId" validate:"required"`
+	MovieID   string  `json:"movieId" validate:"required"`
+	PaymentID *string `json:"paymentId"`
+}
+
+type UpdateBookingPaymentRequest struct {
+	ID        string  `json:"id" validate:"required"`
+	PaymentID *string `json:"paymentId" validate:"required"`
+}
